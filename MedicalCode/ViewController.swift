fda731ebf8ac8ff2b0d56f39c94cbfb2c29aeb1c
Ibrahim Ahmed El-geddawy <ielgeddawy@gmail.com>
2018-12-16 20:16:35 +0200

    Initial Commit

---
+++ MedicalCode/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MedicalCode
+//
+//  Created by Ibrahim El-geddawy on 12/16/18.
+//  Copyright © 2018 Ibrahim El-geddawy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
